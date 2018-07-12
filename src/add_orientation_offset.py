fc5f91e7c72887259143a0bc1136d5e3f26713a1
BrettRStephens <sbrett@student.ethz.ch>
2018-07-12 17:11:17 +0100

    edit orientation to correct msf core

--- src/add_orientation_offset.py
+++ src/add_orientation_offset.py
@@ -8,8 +8,8 @@
 #  Convention:   q_out =  q_in * q_offset
 
 import rospy
-from geometry_msgs.msg import PoseStamped
-import tf.transformations as tf
+from nav_msgs.msg import Odometry
+import tf
 import math
 
 
@@ -18,9 +18,9 @@ class AddOrientationOffset:
 
         self.i = 0
 
-        rospy.Subscriber('maplab_rovio/T_G_I', PoseStamped, self.pose_callback)
+        rospy.Subscriber('maplab_rovio/T_G_I', Odometry, self.pose_callback)
 
-        self.pub_pose_out = rospy.Publisher('maplab_rovio/T_G_I/init',PoseStamped, queue_size=10)
+        self.pub_pose_out = rospy.Publisher('maplab_rovio/T_G_I/init',Odometry, queue_size=10)
 
         rospy.spin()
 
@@ -30,12 +30,12 @@ class AddOrientationOffset:
 
         if self.i == 1:
             print ("add_orientation_offset initialized")
-            q_in = [msg.pose.orientation.x,
-                           msg.pose.orientation.y,
-                           msg.pose.orientation.z,
-                           msg.pose.orientation.w]
+            q_in = [msg.pose.pose.orientation.x,
+                    msg.pose.pose.orientation.y,
+                    msg.pose.pose.orientation.z,
+                    msg.pose.pose.orientation.w]
 
-            euler_init_rpy = tf.euler_from_quaternion(q_in)
+            euler_init_rpy = tf.transformations.euler_from_quaternion(q_in)
             roll = euler_init_rpy[0]
             pitch = euler_init_rpy[1]
             yaw = euler_init_rpy[2]
@@ -47,23 +47,30 @@ class AddOrientationOffset:
             pitch_out = pitch + self.pitch_offset
             yaw_out = yaw + self.yaw_offset
 
-            q_new = tf.quaternion_from_euler(roll_out,pitch_out,yaw_out)
+            q_new = tf.transformations.quaternion_from_euler(roll_out,pitch_out,yaw_out)
 
             pose_out = msg
-            pose_out.pose.orientation.x = q_new[0]
-            pose_out.pose.orientation.y = q_new[1]
-            pose_out.pose.orientation.z = q_new[2]
-            pose_out.pose.orientation.w = q_new[3]
+            pose_out.pose.pose.orientation.x = q_new[0]
+            pose_out.pose.pose.orientation.y = q_new[1]
+            pose_out.pose.pose.orientation.z = q_new[2]
+            pose_out.pose.pose.orientation.w = q_new[3]
+
+            transform_pub = tf.TransformBroadcaster()
+            transform_pub.sendTransform((pose_out.pose.pose.position.x, pose_out.pose.pose.position.y, pose_out.pose.pose.position.z),
+                     q_new,
+                     rospy.Time.now(),
+                     "state_corrected",
+                     "world")
 
             self.pub_pose_out.publish(pose_out) 
 
         else:
-            q_in = [msg.pose.orientation.x,
-                           msg.pose.orientation.y,
-                           msg.pose.orientation.z,
-                           msg.pose.orientation.w]
+            q_in = [msg.pose.pose.orientation.x,
+                    msg.pose.pose.orientation.y,
+                    msg.pose.pose.orientation.z,
+                    msg.pose.pose.orientation.w]
 
-            euler_in_rpy = tf.euler_from_quaternion(q_in)
+            euler_in_rpy = tf.transformations.euler_from_quaternion(q_in)
             roll = euler_in_rpy[0]
             pitch = euler_in_rpy[1]
             yaw = euler_in_rpy[2]
@@ -72,13 +79,20 @@ class AddOrientationOffset:
             pitch_out = pitch + self.pitch_offset
             yaw_out = yaw + self.yaw_offset
 
-            q_new = tf.quaternion_from_euler(roll_out,pitch_out,yaw_out)
+            q_new = tf.transformations.quaternion_from_euler(roll_out,pitch_out,yaw_out)
 
             pose_out = msg
-            pose_out.pose.orientation.x = q_new[0]
-            pose_out.pose.orientation.y = q_new[1]
-            pose_out.pose.orientation.z = q_new[2]
-            pose_out.pose.orientation.w = q_new[3]
+            pose_out.pose.pose.orientation.x = q_new[0]
+            pose_out.pose.pose.orientation.y = q_new[1]
+            pose_out.pose.pose.orientation.z = q_new[2]
+            pose_out.pose.pose.orientation.w = q_new[3]
+
+            transform_pub = tf.TransformBroadcaster()
+            transform_pub.sendTransform((pose_out.pose.pose.position.x, pose_out.pose.pose.position.y, pose_out.pose.pose.position.z),
+                     q_new,
+                     rospy.Time.now(),
+                     "state_corrected",
+                     "world")
 
             self.pub_pose_out.publish(pose_out) 
 
